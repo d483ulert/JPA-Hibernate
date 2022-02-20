f1a6b7408d660e24ceb67cb7d0fef22c515e6f87
jisung324 <jisung3245@gmail.com>
2022-02-21 01:31:19 +0900

    Initial commit

---
+++ src/main/java/com/my/toyprj/ToyprjApplication.java
@@ -0,0 +1,13 @@
+package com.my.toyprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToyprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToyprjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/my/toyprj/ToyprjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.my.toyprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToyprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

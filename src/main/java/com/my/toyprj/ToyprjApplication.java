6b9fec6a6c3a3b6e3bc9fedfbfd74fec4fdcfe8a
jisung324 <jisung3245@gmail.com>
2022-02-23 20:49:16 +0900

    database info

--- src/main/java/com/my/toyprj/ToyprjApplication.java
+++ src/main/java/com/my/toyprj/ToyprjApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ToyprjApplication {
-
 	public static void main(String[] args) {
+		System.out.println("ss");
 		SpringApplication.run(ToyprjApplication.class, args);
 	}
 

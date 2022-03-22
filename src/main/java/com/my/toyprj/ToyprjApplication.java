5fcb858c0edd25346f594c9c7c17b8156f35fa52
jisung324 <jisung3245@gmail.com>
2022-03-22 23:00:12 +0900

    fragment commit.

--- src/main/java/com/my/toyprj/ToyprjApplication.java
+++ src/main/java/com/my/toyprj/ToyprjApplication.java
@@ -2,17 +2,23 @@ package com.my.toyprj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.CacheControl;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @SpringBootApplication
-public class ToyprjApplication {
+public class ToyprjApplication implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(final ResourceHandlerRegistry registry){
+		registry.addResourceHandler("/**").addResourceLocations("classpath:/templates/")
+		.setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ToyprjApplication.class, args);
 	}
 
-	@RequestMapping("/main")
-	public String main(){
-
-		return "Main";
-	}
 }
--- src/main/java/com/my/toyprj/board/controller/BoardController.java
+++ src/main/java/com/my/toyprj/board/controller/BoardController.java
@@ -1,18 +1,19 @@
 package com.my.toyprj.board.controller;
+import com.my.toyprj.ToyprjApplication;
 import com.my.toyprj.board.dto.BoardDTO;
 import com.my.toyprj.board.entity.BoardEntity;
 import com.my.toyprj.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
 public class BoardController {
-
     @Autowired
     private BoardRepository boardRepository;
 

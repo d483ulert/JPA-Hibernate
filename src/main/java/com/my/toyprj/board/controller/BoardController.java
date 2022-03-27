d6fb84ce8b9aacce1ed163baeaddd106cdd283ce
jisung324 <jisung3245@gmail.com>
2022-03-27 22:08:17 +0900

    list modify

--- src/main/java/com/my/toyprj/board/controller/BoardController.java
+++ src/main/java/com/my/toyprj/board/controller/BoardController.java
@@ -38,6 +38,8 @@ public class BoardController {
 
         BoardEntity entity = boardDTO.toEntity();
 
+        System.out.println("brdentt"+entity);
+
         BoardEntity saved = boardRepository.save(entity);
     }
 

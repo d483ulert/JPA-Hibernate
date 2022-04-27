adcccf514df2b78e7fae04e844e4eadb7c9d11a5
jisung324 <jisung3245@gmail.com>
2022-04-27 21:38:05 +0900

    회원가입 view 개발진행중

--- src/main/java/com/my/toyprj/member/controller/MemberController.java
+++ src/main/java/com/my/toyprj/member/controller/MemberController.java
@@ -3,7 +3,12 @@ package com.my.toyprj.member.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.net.http.HttpRequest;
+import java.util.HashMap;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,6 +21,15 @@ public class MemberController {
 
         return "member/join";
     }
+
+    @PostMapping("/pwdVal")
+    public HashMap pwd(@RequestParam String pwd, @RequestParam String pwd2){
+        HashMap<String,String> hmap = new HashMap<>();
+
+        return hmap;
+    }
+
+
 
 
 }

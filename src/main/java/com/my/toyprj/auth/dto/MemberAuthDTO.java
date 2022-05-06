9a67a7071c3541535dca25e883ab0f3b71452bcc
jisung324 <jisung3245@gmail.com>
2022-05-06 21:26:07 +0900

    MemberAuth 디렉토리 추가

--- src/main/java/com/my/toyprj/auth/dto/MemberAuthDTO.java
+++ src/main/java/com/my/toyprj/auth/dto/MemberAuthDTO.java
@@ -1,43 +1,35 @@
-package com.my.toyprj.member.entity;
-
-import lombok.Builder;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+package com.my.toyprj.auth.dto;
+import com.my.toyprj.auth.entity.MemberAuth;
+import lombok.*;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
-@Entity
 @Getter
+@Setter
+@ToString
 @NoArgsConstructor
-@EqualsAndHashCode(of="userNo")
-public class MemberAuth {
+public class MemberAuthDTO {
 
-    @Column
-    @GeneratedValue
-    @Id
     private Long userAuthNo;
 
-    @Column
     private Long userNo;
 
-    @Column
     private String auth;
 
-    @Column
     private LocalDateTime regDate;
 
-    @Column
     private LocalDateTime upDate;
 
     @Builder
-    public MemberAuth(Long userNo, String auth){
+    public MemberAuthDTO(Long userNo, String auth){
         this.userNo =userNo;
         this.auth=auth;
     }
 
+    public MemberAuth toEntity(Long userNo, String auth){
+        return new MemberAuth(userNo,auth);
+    }
 }
---
+++ src/main/java/com/my/toyprj/auth/entity/MemberAuth.java
@@ -0,0 +1,44 @@
+package com.my.toyprj.auth.entity;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@EqualsAndHashCode(of="userNo")
+public class MemberAuth {
+
+    @Column
+    @GeneratedValue
+    @Id
+    private Long userAuthNo;
+
+    @Column
+    private Long userNo;
+
+    @Column
+    private String auth;
+
+    @Column
+    private LocalDateTime regDate;
+
+    @Column
+    private LocalDateTime upDate;
+
+    @Builder
+    public MemberAuth(Long userNo, String auth){
+        this.userNo =userNo;
+        this.auth=auth;
+    }
+
+
+}

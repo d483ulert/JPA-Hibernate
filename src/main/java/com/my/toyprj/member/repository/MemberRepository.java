6831b4d6e8375572060f89857346f4df36276efd
jisung324 <jisung3245@gmail.com>
2022-07-20 21:37:45 +0900

    jwt 관련 권한수정

--- src/main/java/com/my/toyprj/member/repository/MemberRepository.java
+++ src/main/java/com/my/toyprj/member/repository/MemberRepository.java
@@ -1,8 +1,12 @@
 package com.my.toyprj.member.repository;
 import com.my.toyprj.member.entity.Member;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member,Long> {
 
-    public Member findByUserId(String userId);
+    @EntityGraph(attributePaths = "authorities")
+    Optional<Member> findOneWithAuthoritiesByUsername(String userId);
 }
--- src/main/java/com/my/toyprj/security/config/CustomUserDetailService.java
+++ src/main/java/com/my/toyprj/security/config/CustomUserDetailService.java
@@ -1,24 +1,44 @@
 package com.my.toyprj.security.config;
 
-import com.my.toyprj.member.entity.Member;
 import com.my.toyprj.member.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
-@Service
+@Component("UserDetailsService")
 public class CustomUserDetailService implements UserDetailsService {
 
     @Autowired
     private MemberRepository memberRepository;
 
+    public CustomUserDetailService(MemberRepository memberRepository){
+        this.memberRepository=memberRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
-        Member member = memberRepository.findByUserId(userId);
-        System.out.println("member:"+member);
-        return new CustomUser(member);
+    public UserDetails loadUserByUsername(final String userId)  {
+        return memberRepository.findOneWithAuthoritiesByUsername(userId)
+                .map(member-> createMember(userId,member))
+                .orElseThrow(() -> new UsernameNotFoundException(userId + "null"));
+    }
+
+    private org.springframework.security.core.userdetails.User createMember(String username, Object member) {
+        if (!member.isActivated()) {
+            throw new RuntimeException(username + " -> 활성화되어 있지 않습니다.");
+        }
+        List<GrantedAuthority> grantedAuthorities = member.getAuthorities().stream()
+                .map(authority -> new SimpleGrantedAuthority(authority.getAuthorityName()))
+                .collect(Collectors.toList());
+        return new org.springframework.security.core.userdetails.User(member.getUsername(),
+                member.getPassword(),
+                grantedAuthorities);
     }
 }

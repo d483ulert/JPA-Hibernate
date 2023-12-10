cafb19d83fc267e226de16b80acea9baa1fb9a52
d483ulert <jisung7153@naver.com>
2023-12-10 20:49:51 +0900

    source tree commit

---
+++ src/main/java/com/my/toyprj/auth/dto/MemberDTO.java
@@ -0,0 +1,32 @@
+package com.my.toyprj.auth.dto;
+import com.my.toyprj.auth.entity.MemberAuth;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class MemberAuthDTO {
+
+    private Long userAuthNo;
+
+    private Long userNo;
+
+    private String auth;
+
+    private LocalDateTime regDate;
+
+    private LocalDateTime upDate;
+
+    @Builder
+    public MemberAuthDTO(Long userNo, String auth){
+        this.userNo =userNo;
+        this.auth=auth;
+    }
+
+    public MemberAuth toEntity(Long userNo, String auth){
+        return new MemberAuth(userNo,auth);
+    }
+}
